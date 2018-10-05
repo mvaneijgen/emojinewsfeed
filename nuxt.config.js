0bcd0196ee92da2fa6d46c8708273c0a835403bd
Mitchel van Eijgen <mvaneijgen@gmail.com>
2018-10-05 18:00:45 +0200

    Moved to custom domain base url fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const routerBase =
   process.env.DEPLOY_ENV === "GH_PAGES"
     ? {
         router: {
-          base: "/emojinewsfeed/",
+          // base: "/emojinewsfeed/",
         },
       }
     : {};
